c642f82d22ecb5548ecc329e079d40e0e9152d27
FabiOquendo <fabioquendo95@gmail.com>
2021-04-12 18:40:02 -0500

    Indentation

--- DeskBooker/DeskBooker.Web/Startup.cs
+++ DeskBooker/DeskBooker.Web/Startup.cs
@@ -11,68 +11,68 @@ using Microsoft.Extensions.Hosting;
 
 namespace DeskBooker.Web
 {
-  public class Startup
-  {
-    public Startup(IConfiguration configuration)
+    public class Startup
     {
-      Configuration = configuration;
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRazorPages();
+
+            var connectionString = "DataSource=:memory:";
+            var connection = new SqliteConnection(connectionString);
+            connection.Open();
+
+            services.AddDbContext<DeskBookerContext>(options =>
+                options.UseSqlite(connection)
+            );
+
+            EnsureDatabaseExists(connection);
+
+            services.AddTransient<IDeskRepository, DeskRepository>();
+            services.AddTransient<IDeskBookingRepository, DeskBookingRepository>();
+        }
+
+        private static void EnsureDatabaseExists(SqliteConnection connection)
+        {
+            var builder = new DbContextOptionsBuilder<DeskBookerContext>();
+            builder.UseSqlite(connection);
+
+            using var context = new DeskBookerContext(builder.Options);
+            context.Database.EnsureCreated();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapRazorPages();
+            });
+        }
     }
-
-    public IConfiguration Configuration { get; }
-
-    // This method gets called by the runtime. Use this method to add services to the container.
-    public void ConfigureServices(IServiceCollection services)
-    {
-      services.AddRazorPages();
-
-      var connectionString = "DataSource=:memory:";
-      var connection = new SqliteConnection(connectionString);
-      connection.Open();
-
-      services.AddDbContext<DeskBookerContext>(options =>
-          options.UseSqlite(connection)
-      );
-
-      EnsureDatabaseExists(connection);
-
-      services.AddTransient<IDeskRepository, DeskRepository>();
-      services.AddTransient<IDeskBookingRepository, DeskBookingRepository>();
-    }
-
-    private static void EnsureDatabaseExists(SqliteConnection connection)
-    {
-      var builder = new DbContextOptionsBuilder<DeskBookerContext>();
-      builder.UseSqlite(connection);
-
-      using var context = new DeskBookerContext(builder.Options);
-      context.Database.EnsureCreated();
-    }
-
-    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-    {
-      if (env.IsDevelopment())
-      {
-        app.UseDeveloperExceptionPage();
-      }
-      else
-      {
-        app.UseExceptionHandler("/Error");
-        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-        app.UseHsts();
-      }
-
-      app.UseHttpsRedirection();
-      app.UseStaticFiles();
-
-      app.UseRouting();
-
-      app.UseAuthorization();
-
-      app.UseEndpoints(endpoints =>
-      {
-        endpoints.MapRazorPages();
-      });
-    }
-  }
 }
