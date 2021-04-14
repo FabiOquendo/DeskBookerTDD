e8858d95ee23cf7edfac2ea0dbc7da11307c5d27
FabiOquendo <fabioquendo95@gmail.com>
2021-04-13 19:04:09 -0500

    Test and Implement: Call the BookDesk Method of the Processor

--- DeskBooker.Web.Tests/Pages/BookDeskModelTests.cs
+++ DeskBooker.Web.Tests/Pages/BookDeskModelTests.cs
@@ -1,7 +1,28 @@
-﻿namespace DeskBooker.Web.Pages
+﻿using DeskBooker.Core.Domain;
+using DeskBooker.Core.Processor;
+using Moq;
+using Xunit;
+
+namespace DeskBooker.Web.Pages
 {
     public class BookDeskModelTests
     {
+        [Fact]
+        public void ShouldCallBookDeskMethodOfProcessor()
+        {
+            // Arrange
+            var processorMock = new Mock<IDeskBookingRequestProcessor>();
 
+            var bookDeskModel = new BookDeskModel(processorMock.Object)
+            {
+                DeskBookingRequest = new DeskBookingRequest()
+            };
+
+            // Act
+            bookDeskModel.OnPost();
+
+            // Assert
+            processorMock.Verify(x => x.BookDesk(bookDeskModel.DeskBookingRequest), Times.Once);
+        }
     }
 }
--- DeskBooker.Web/Pages/BookDesk.cshtml.cs
+++ DeskBooker.Web/Pages/BookDesk.cshtml.cs
@@ -1,17 +1,25 @@
 ﻿using DeskBooker.Core.Domain;
+using DeskBooker.Core.Processor;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace DeskBooker.Web.Pages
 {
-  public class BookDeskModel : PageModel
-  {
-    [BindProperty]
-    public DeskBookingRequest DeskBookingRequest { get; set; }
-
-    public void OnPost()
+    public class BookDeskModel : PageModel
     {
-      
+        private IDeskBookingRequestProcessor _deskBookingRequestProcessor;
+
+        public BookDeskModel(IDeskBookingRequestProcessor deskBookingRequestProcessor)
+        {
+            _deskBookingRequestProcessor = deskBookingRequestProcessor;
+        }
+
+        [BindProperty]
+        public DeskBookingRequest DeskBookingRequest { get; set; }
+
+        public void OnPost()
+        {
+            _deskBookingRequestProcessor.BookDesk(DeskBookingRequest);
+        }
     }
-  }
 }
